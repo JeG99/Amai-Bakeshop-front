ad53e67f1fdfabc6aeb11237aa30a55c7e5dc8b5
Sergio Guasso Idalias <sguasso@live.com>
2022-05-14 13:20:03 -0500

    Imagenes

--- src/components/Catalogo/Catalogo.js
+++ src/components/Catalogo/Catalogo.js
@@ -5,6 +5,15 @@ import pastel1 from '../../assets/pastel1.jfif';
 import pastel2 from '../../assets/pastel2.jfif';
 import pastel3 from '../../assets/pastel3.jfif';
 import pastel4 from '../../assets/pastel4.jfif';
+import pastel5 from '../../assets/pastel5.jfif';
+import pastel6 from '../../assets/pastel6.jfif';
+import pastel7 from '../../assets/pastel7.jfif';
+import pastel8 from '../../assets/pastel8.jfif';
+import pastel9 from '../../assets/pastel9.jfif';
+import pastel10 from '../../assets/pastel10.jfif';
+import pastel11 from '../../assets/pastel11.jfif';
+import pastel12 from '../../assets/pastel12.jfif';
+import pastel13 from '../../assets/pastel13.jfif';
 
 function Catalogo() {
   return (
@@ -41,7 +50,7 @@ function Catalogo() {
                         <Card.Body>
                             <Card.Title>Pastel de fresa</Card.Title>
                             <Card.Text>
-                            Precio: $650
+                            Precio: $500
                             </Card.Text>
                             <Button variant="primary">Añadir al carrito</Button>
                         </Card.Body>
@@ -54,6 +63,114 @@ function Catalogo() {
                             <Card.Title>Pastel de fresa</Card.Title>
                             <Card.Text>
                             Precio: $650
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel5} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $350
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel6} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $nose
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel7} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $nose
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel8} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $nose
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel9} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $$nose
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel10} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $$nose
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel11} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $$nose
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel12} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $250
+                            </Card.Text>
+                            <Button variant="primary">Añadir al carrito</Button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="col-md-4">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Img variant="top" src={pastel13} />
+                        <Card.Body>
+                            <Card.Title>Pastel de fresa</Card.Title>
+                            <Card.Text>
+                            Precio: $350
                             </Card.Text>
                             <Button variant="primary">Añadir al carrito</Button>
                         </Card.Body>
