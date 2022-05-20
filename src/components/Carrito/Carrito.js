43196e7c585eeffc5c32190bd98a05e2f0f2d682
JuanPabdl <a00825316@itesm.mx>
2022-05-20 17:26:34 -0500

    edited carrito

--- src/components/Carrito/Carrito.js
+++ src/components/Carrito/Carrito.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { ListGroup } from "react-bootstrap";
 import { Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 
 class Carrito extends React.Component{
     constructor(props){
@@ -14,29 +15,64 @@ class Carrito extends React.Component{
     }
 
     deleteOrder(i){
+        this.state.lista_carrito.splice(i,1)
+    }
+
+    payOrder(){
     }
 
     render(){
         return(
             <div>
-                <div>
-                    <h2>Carrito de compras</h2>
-                </div>
-                <ListGroup as="ul">
-                    <ListGroup.Item as="li" active>
-                        Lista del Carrito
-                    </ListGroup.Item>
-                    {/* Abajo se generara la lista de productos del carrito */}
-                    <ListGroup.Item as="li">
-                        <div>
-                            <h5>Nombre de Producto</h5>
-                            <Button className="danger">Eliminar</Button>
-                        </div>
-                        <div>
-                            <p>Detalles de la orden</p>
-                        </div>
-                    </ListGroup.Item>
-                </ListGroup>
+                <Container className="w-75">
+                    <Row className="mt-3 d-flexbox justify-content-center">
+                        <Col xs={12} sm={4} md={4}><h2>Carrito de compras</h2></Col>
+                    </Row>
+                    <Row className="mt-3">
+                        <Col>
+                            <ListGroup as="ul">
+                                <ListGroup.Item as="li">
+                                    <h3>Lista del Carrito</h3>
+                                </ListGroup.Item>
+                                {/* Abajo se generara la lista de productos del carrito */}
+                                <ListGroup.Item as="li">
+                                    <Container>
+                                        <Row>
+                                            <Col><h5>Nombre de Producto</h5></Col>
+                                            <Col>
+                                                <div className="d-flex flex-row-reverse">
+                                                    <Button variant="danger">Eliminar</Button>
+                                                </div>
+                                            </Col>
+                                        </Row>
+                                        <Row className="mt-3">
+                                            <Col>
+                                                <p>Cantidad: X</p>
+                                                <p>Costo: X</p>
+                                            </Col>
+                                            
+                                        </Row>
+                                    </Container>
+                                </ListGroup.Item>
+                                {/* Espacio donde se calcula la cantidad total de la orden */}
+                                <ListGroup.Item as="li">
+                                    <div className="d-flexbox justify-content-center">
+                                        <p>Total: X</p>
+                                    </div>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="mt-3">
+                            <div className="d-flexbox justify-content-center">
+                                <Button id="btnPago" size="lg">
+                                    Pagar
+                                </Button>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
             </div>
         )
     }
